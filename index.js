0a9c3159812bd5e99ca2bc32d8ba3952106779c5
mashhurbek <mashhurraximov59@gmail.com>
2025-11-15 15:50:17 +0500

    telegrambot

---
+++ index.js
@@ -0,0 +1,84 @@
+import TelegramBot from "node-telegram-bot-api";
+
+const TOKEN = "8333719530:AAHyNanhsuFYvsP1c2Om-pbrWef1_ic-_M0";
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.on("message", (msg) => {
+  console.log(msg);
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  const firstName = msg.chat.first_name;
+
+  if (text == "/start" ) {
+    bot.sendMessage(
+      chatId,
+      `
+    👋 Assalomu alaykum, ${firstName}!
+
+📚 100x o‘quv markazining rasmiy botiga xush kelibsiz!
+
+Bu bot orqali siz:
+• Kurslarimiz haqida batafsil ma’lumot olasiz  
+• Kurslarga onlayn ro‘yxatdan o‘tishingiz mumkin  
+• Jadval va to‘lovlar haqida ma’lumot olasiz  
+
+Quyidagi menyudan kerakli bo‘limni tanlang 👇
+
+    `,
+      {
+        reply_markup: {
+          keyboard: [
+            [{ text: "📚 Kurslar" }, { text: "✍️ Ro‘yxatdan o‘tish" }],
+            [{ text: "ℹ️ Markaz haqida" }, { text: "💬 Fikr bildirish" }],
+            [{ text: "❓ Yordam" }],
+          ],
+          resize_keyboard: true,
+        },
+      }
+    );
+  } else if (text == "📚 Kurslar") {
+    bot.sendMessage(
+      chatId,
+      `
+    🎓 Bizning o‘quv markazimizda quyidagi kurslar mavjud:
+
+1️⃣ Ingliz tili  
+2️⃣ Rus tili  
+3️⃣ Matematika  
+4️⃣ Dasturlash (Python, Web)  
+5️⃣ Grafik dizayn  
+
+👇 Quyidagi kurslardan birini tanlang va batafsil ma’lumot oling:
+
+    `,
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "🇬🇧 Ingliz tili", callback_data: "course_english" }],
+            [{ text: "🇷🇺 Rus tili", callback_data: "course_russian" }],
+            [{ text: "🧮 Matematika", callback_data: "course_math" }],
+            [{ text: "💻 Dasturlash", callback_data: "course_programming" }],
+            [{ text: "🎨 Grafik dizayn", callback_data: "course_design" }],
+          ],
+        },
+      }
+    );
+  } else if (text == "/location") {
+    const latitude = 41.38705;
+    const longitude = 60.36270;
+
+    bot.sendMessage(chatId, "📍 Bizning o‘quv markaz joylashuvi:");
+    bot.sendLocation(chatId, latitude, longitude);
+   } else {
+    bot.sendMessage(
+      chatId,
+      `
+    ⚠️ Kechirasiz, men sizning xabaringizni tushunmadim.
+
+Iltimos, quyidagi tugmani bosing 👇
+/start
+    `
+    );
+  }
+});
+
